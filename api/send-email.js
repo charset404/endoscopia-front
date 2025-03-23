972e47fbabb7b218685acb74a38afa68c21370d7
charset404 <sobrelevel@gmail.com>
2025-03-23 19:33:36 -0300

    Fix: vercel api 2

--- api/send-email.js
+++ api/send-email.js
@@ -1,16 +1,17 @@
-const emailjs = require("@emailjs/browser");
+import emailjs from "@emailjs/browser";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    const formData = req.body;
+    const formData = req.body; // Dados do formulário recebidos do Front-End
 
     try {
-      emailjs.init("UDd8kCDYeoyRHPhWs");
+      emailjs.init("UDd8kCDYeoyRHPhWs"); // Inicia com a chave pública do EmailJS
 
+      // Envia o e-mail com os dados do formulário usando emailjs.send()
       const response = await emailjs.send(
-        "service_9itshwl",
-        "template_contact",
-        formData
+        "service_9itshwl", // Seu ID do serviço
+        "template_contact", // Seu ID do template
+        formData // Os dados do formulário
       );
 
       return res.status(200).json({ message: "E-mail enviado com sucesso!" });
