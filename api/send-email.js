c28fde15f2f6022bd2f63beb3d9230d8d660cc1e
charset404 <sobrelevel@gmail.com>
2025-03-23 19:24:31 -0300

    Feat: emailjs integration & theme color change

---
+++ api/send-email.js
@@ -0,0 +1,26 @@
+// api/send-email.js
+
+const emailjs = require("@emailjs/browser");
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { formData } = req.body;
+
+    try {
+      emailjs.init("UDd8kCDYeoyRHPhWs");
+
+      const response = await emailjs.sendForm(
+        "service_9itshwl",
+        "template_contact",
+        formData
+      );
+
+      return res.status(200).json({ message: "E-mail enviado com sucesso!" });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: "Erro ao enviar o e-mail!" });
+    }
+  } else {
+    return res.status(405).json({ message: "Método não permitido" });
+  }
+}
